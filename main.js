de61b79dde6c045ed3bde3f7f1e1e2ec27694bbc
Jhonatan <jhonatanmedina1590@gmail.com>
2020-12-01 23:01:20 -0800

    fixed header layout, zip code search bar, and local search

--- main.js
+++ main.js
@@ -1,52 +1,55 @@
 const size = "150";
+// API KEY 
+var key = 'f5a76bcde0f4fed7682b28cd60f2f9b1';
 //...... Date function.................................
 // Function sets the dates for the each day receiving as input
 // how many days we need to increase. 
 
-  function addDays(days) {
-    const dayArray = ["Sun","Mon","Tue","Wed","Thur","Fri","Sat"];
-    const monthArray = ["Jan","Feb","Mar","May","Jun","July","Aug","Sep","Oct","Nov","Dec"];
+function addDays(days) {
+  const dayArray = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+  const monthArray = ["Jan", "Feb", "Mar", "May", "Jun", "July", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
-    function amPm (hour){
-      if(hour>12){return "PM";}
-      else return "AM";
-    }
-
-    const currentDate = new Date();
-
-    const hour = hoursIn12(currentDate.getHours());
-    const minute = currentDate.getMinutes();
-    const day = currentDate.getDay();
-    const month = currentDate.getMonth();
-    const year = currentDate.getFullYear();
-
-    const result = hour+":"+minute+" "+amPm(currentDate.getHours())+" "+dayArray[day]+" "+monthArray[month]+" "+year;
-
-    return result;
+  function amPm(hour) {
+    if (hour > 12) { return "PM"; }
+    else return "AM";
   }
 
-  // Turn Hours into 12 hour format
+  const currentDate = new Date();
+
+  const hour = hoursIn12(currentDate.getHours());
+  const minute = currentDate.getMinutes();
+  const day = currentDate.getDay();
+  const month = currentDate.getMonth();
+  const year = currentDate.getFullYear();
+
+  const result = hour + ":" + minute + " " + amPm(currentDate.getHours()) + " " + dayArray[day] + " " + monthArray[month] + " " + year;
+
+  return result;
+}
+
+// Turn Hours into 12 hour format
 console.log(addDays(1));
 
-function hoursIn12(hours){
-  const hour12 = ((hours+11)%12+1);
+function hoursIn12(hours) {
+  const hour12 = ((hours + 11) % 12 + 1);
   return hour12;
 }
 
 // gets temperature in Farengeit
-function fahrenheit(temp){
-  return Math.round(((parseFloat(temp)-273.15)*1.8)+32)
+function fahrenheit(temp) {
+  return Math.round(((parseFloat(temp) - 273.15) * 1.8) + 32)
 }
 // gets temperature in Celcius
-function celcius(temp){
-  return Math.round(parseFloat(fahrenheit(temp)-32)/(9/5));
+function celcius(temp) {
+  return Math.round(parseFloat(fahrenheit(temp) - 32) / (9 / 5));
 }
 //.............. direction..................................
 
 function convertWindDir(deg) {
-  let compass = ["N","NNE","NE","ENE","E","ESE","SE","SSE","S","SSW","SW","WSW","W","WNW","NW","NNW","N"]
-  let index = Math.round((deg % 360) / 22.5 )
-  return compass[index]}
+  let compass = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW", "N"]
+  let index = Math.round((deg % 360) / 22.5)
+  return compass[index]
+}
 ///............. Main divs in page....................
 
 //make sure its right div in array
@@ -55,97 +58,155 @@ const todaydiv = document.getElementById("today");//Div with todays weather
 console.log(todaydiv);
 
 ///.........................................................
-
-
-//.....................WeekDays ................................
-//Loops through the array and increases the dates of each day
-// of the week for 6 days following the the current date
-//Temporary Weather Api Key
-
-async function weatherBalloon( cityID,pos=0 ) {
-
-  var key = 'f5a76bcde0f4fed7682b28cd60f2f9b1';
-  fetch('https://api.openweathermap.org/data/2.5/weather?zip=' + cityID+ '&appid=' + key)  
+function currentLocation(){
+  function success(position) {
+    const latitude  = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    console.log(latitude, longitude);
+    weatherlocation(latitude, longitude);
+  }
   
-  .then(function(resp) { return resp.json() }) // Convert data to json
-  .then(function(data) {
-  console.log(data);
-///...................Today Main div.........
-//Sets date for today
-        const temp = fahrenheit(data.main.temp) +"F°/"+celcius(data.main.temp)+"C°";
-        const lowTemp = fahrenheit(data.main.temp_min) +"F°/"+celcius(data.main.temp_min)+"C°";
-        const highTemp = fahrenheit(data.main.temp_max) +"F°/"+celcius(data.main.temp_max)+"C°"
-        const feelsLike = fahrenheit(data.main.feels_like) +"F°/"+celcius(data.main.feels_like)+"C°"
-        const sunRise = new Date(data.sys.sunrise*1000);
-        const sunSet = new Date(data.sys.sunset*1000);
-        const visibility = data.visibility + "-ft";
-        
-        const humidity = data.main.humidity;
-        const wind = data.wind.speed +" MPH "+ convertWindDir(data.wind.deg); // convert to KM
-        const myIcon = data.weather[0].icon;
-        //........................Today's Weather........................
-        document.getElementById("Date").innerHTML = addDays(0);
-       
-        document.getElementById("Description").innerHTML=data.weather[0].description;
-        document.getElementById("Location").innerHTML=data.name;
-        document.getElementById("Temp").innerHTML = temp;
-        
-        //...........................Bottom Icons.................................
-        document.getElementById("lowTemp").innerHTML=lowTemp;
-        document.getElementById("highTemp").innerHTML=highTemp;
-        document.getElementById("wind").innerHTML= wind;
-        document.getElementById("local").
-        document.getElementById("humidity").innerHTML=humidity+" %";
-        document.getElementById("feelsLike").innerHTML= feelsLike;
-        document.getElementById("visibility").innerHTML= visibility;
-        
+  function error() {
+    status.textContent = 'Unable to retrieve your location';
+    weatherZip(90005);
+  }
+  
+  if(!navigator.geolocation) {
+    status.textContent = 'Geolocation is not supported by your browser';
+  } else {
+    status.textContent = 'Locating…';
+    navigator.geolocation.getCurrentPosition(success, error);
+  }
+}
+//.......................................................................
+var locationHistory = {
+  //Information will be added when program runs
+  
+};
+
+//.............................................................................
+async function weatherID(cityID, pos = 0) {
+  //Fetch to get the weather data by altitude and longitude
+  
+  fetch("https://api.openweathermap.org/data/2.5/group?id="+cityID+"&appid="+key) 
+
+    .then(function (resp) { return resp.json() }) // Convert data to json
+    .then(function (data) {
+      console.log(data);
+      ///...................Today Main div.........
+      //Sets date for today
+      applyToHtml(data);
+    })
+    .catch(function () {
       
-        document.getElementById("sunRise").innerHTML = hoursIn12(sunRise.getHours())+":"+sunRise.getHours()+" AM";
-        document.getElementById("sunSet").innerHTML  = hoursIn12(sunSet.getHours())+":"+sunSet.getMinutes()+" PM";
-        //..........................Weather Icon..................................
-        const imgsun = document.getElementById("wicon");
-        imgsun.src="http://openweathermap.org/img/w/"+ myIcon+".png";
-        imgsun.width = size;
-        imgsun.height = size;
-        
-        
-  })
-  .catch(function() {
-    // catch any errors
-  });
+    });
+}
+
+//.............................................................................
+async function weatherlocation(myLat,myLong, pos = 0) {
+  //Fetch to get the weather data by altitude and longitude
+  
+  fetch("https://api.openweathermap.org/data/2.5/weather?lat="+myLat+"&lon="+ myLong+ "&appid=" + key) 
+
+    .then(function (resp) { return resp.json() }) // Convert data to json
+    .then(function (data) {
+      console.log(data);
+      ///...................Today Main div.........
+      //Sets date for today
+      applyToHtml(data);
+    })
+    .catch(function () {
+      
+    });
+}
+
+//.........................................................................
+async function weatherZip(myZip, pos = 0) {
+// Fetch the weather data using a zipcode
+  
+  fetch('https://api.openweathermap.org/data/2.5/weather?zip=' + myZip + '&appid=' + key)
+
+    .then(function (resp) { return resp.json() }) // Convert data to json
+    .then(function (data) {
+      console.log(data);
+      applyToHtml(data);
+      ///...................Today Main div.........
+    })
+    .catch(function () {
+      // catch any errors
+      //If the zipcode function fails we try the current location aproach
+      
+    });
+}
+
+function applyToHtml(data){
+  const temp = fahrenheit(data.main.temp) + "F°/" + celcius(data.main.temp) + "C°";
+      const lowTemp = fahrenheit(data.main.temp_min) + "F°/" + celcius(data.main.temp_min) + "C°";
+      const highTemp = fahrenheit(data.main.temp_max) + "F°/" + celcius(data.main.temp_max) + "C°"
+      const feelsLike = fahrenheit(data.main.feels_like) + "F°/" + celcius(data.main.feels_like) + "C°"
+      const sunRise = new Date(data.sys.sunrise * 1000);
+      const sunSet = new Date(data.sys.sunset * 1000);
+      const visibility = data.visibility + "-ft";
+
+      const humidity = data.main.humidity;
+      const wind = data.wind.speed + " MPH " + convertWindDir(data.wind.deg); // convert to KM
+      const myIcon = data.weather[0].icon;
+      //........................Today's Weather........................
+      document.getElementById("Date").innerHTML = addDays(0);
+
+      document.getElementById("Description").innerHTML = data.weather[0].description;
+      document.getElementById("Location").innerHTML = data.name;
+      document.getElementById("Temp").innerHTML = temp;
+
+      //...........................Bottom Icons.................................
+      document.getElementById("lowTemp").innerHTML = lowTemp;
+      document.getElementById("highTemp").innerHTML = highTemp;
+      document.getElementById("wind").innerHTML = wind;
+      //document.getElementById("local");
+      document.getElementById("humidity").innerHTML = humidity + " %";
+      document.getElementById("feelsLike").innerHTML = feelsLike;
+      document.getElementById("visibility").innerHTML = visibility;
+
+
+      document.getElementById("sunRise").innerHTML = hoursIn12(sunRise.getHours()) + ":" + sunRise.getHours() + " AM";
+      document.getElementById("sunSet").innerHTML = hoursIn12(sunSet.getHours()) + ":" + sunSet.getMinutes() + " PM";
+      //..........................Weather Icon..................................
+      const imgsun = document.getElementById("wicon");
+      imgsun.src = "http://openweathermap.org/img/w/" + myIcon + ".png";
+      imgsun.width = size;
+      imgsun.height = size;
+
+      // Adding information to the history array
+      const cityname = data.name;
+  
+      locationHistory[cityname]={
+        "longitude":data.coord.lon,
+        "latitude":data.coord.lat,
+        "country":data.sys.country
+      };
+      console.log(locationHistory);
+
 }
 
 // .........................TEST........................................
 //Default area is Los Angeles CA 
 //const defaultZip = navigator.geolocation.pos
-weatherBalloon(90005);
+weatherZip(90005);
 
 //Checks the Zip code when button is clicked
-function checkZipLength(){
+function checkZipLength() {
   var zip = document.getElementById("myInput").value;
-  if(zip.length>=5){
-    weatherBalloon(zip);
+  if (zip.length >= 5) {
+    weatherZip(zip);
   }
   console.log(zip);
 }
 
-// Get zipCode of the current location
-function currentLocation(){
-  if(navigator.geolocation) {
-    const local = navigator.geolocation.getCurrentPosition(function(position) {
-     $("#location").val(position.address.city+", "+position.address.region);
-     weatherBalloon(local);
-     console.log(local);
-  });
- }
-}
+//.....................................................................
 
-
-
-
-
-
-
+//.......................................................................
+const listOfZips = {}
+//sessionStorage.setItem("myLocation", my)
 
 
 function myFunction() {
@@ -166,3 +227,4 @@ function filterFunction() {
     }
   }
 }
+
